27b22576b348342398f7f55d55a0031669aa916c
ahribori <ahribori@gmail.com>
2020-03-10 11:39:10 +0900

    지도 API 지원 표기

---
+++ src/components/layer/Layer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import About from "./components/About"
+
+const Layer = () => {
+  return (
+    <>
+      <About />
+    </>
+  )
+}
+
+export default Layer
--- src/components/layer/components/About.js
+++ src/components/layer/components/About.js
@@ -8,9 +8,13 @@ const useStyles = makeStyles(theme => ({
     right: 0,
     bottom: 0,
     width: 200,
-    fontSize: 11,
     padding: theme.spacing(1),
-    textAlign: "right"
+    textAlign: "right",
+    textShadow: "1px 1px 1px #fff",
+    fontSize: 10,
+    [theme.breakpoints.up("sm")]: {
+      fontSize: 11
+    }
   }
 }))
 
@@ -20,6 +24,7 @@ const About = () => {
     <div className={classes.root}>
       <div>개발: 정현승</div>
       <div>문의: ahribori@gmail.com</div>
+      <div>지도 및 로컬 API 지원 : kakao</div>
     </div>
   )
 }
