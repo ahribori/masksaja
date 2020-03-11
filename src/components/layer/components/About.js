f19e47d181ac8f683a0bc2fdb13fe3265211e891
ahribori <ahribori@gmail.com>
2020-03-11 09:52:28 +0900

    about layer zindex 조정

--- src/components/layer/components/About.js
+++ src/components/layer/components/About.js
@@ -4,7 +4,7 @@ import { makeStyles } from "@material-ui/core/styles"
 const useStyles = makeStyles(theme => ({
   root: {
     position: "fixed",
-    zIndex: 9999,
+    zIndex: 1000,
     right: 0,
     bottom: 0,
     width: 200,
